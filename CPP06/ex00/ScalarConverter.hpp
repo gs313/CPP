d46a6b4d0bdceeb3fe3505b99fb6d7cf803d8aff
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2024-12-23 18:56:36 +0700

    ScalarConverter

--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -1,12 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/19 14:18:29 by scharuka          #+#    #+#             */
-/*   Updated: 2024/12/20 18:29:24 by scharuka         ###   ########.fr       */
+/*   Created: 2024/12/20 18:32:30 by scharuka          #+#    #+#             */
+/*   Updated: 2024/12/20 18:49:06 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+# include <iostream>
+
+class ScalarConverter
+{
+	public:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &copy);
+		~ScalarConverter();
+		ScalarConverter &operator=(const ScalarConverter &copy);
+	private:
+	
+};
+
+std::ostream &operator<<(std::ostream &out, const ScalarConverter &scalarConverter);
+
+#endif
