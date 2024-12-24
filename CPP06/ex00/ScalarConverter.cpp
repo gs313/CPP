a85fd2ff3c88e958593f6bba9052d0ba41062024
gs313 <sirapat1charu@gmail.com>
2024-12-24 23:41:11 +0700

    scinotation start

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 18:47:57 by scharuka          #+#    #+#             */
-/*   Updated: 2024/12/24 20:04:53 by scharuka         ###   ########.fr       */
+/*   Updated: 2024/12/24 23:40:56 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,12 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &copy)
 
 }
 
-static void ScalarConverter::convert(const std::string &str)
+void ScalarConverter::convert(const std::string &str)
+{
+
+}
+
+bool caseSciNotation (std::string)
 {
 	
 }
