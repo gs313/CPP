a4d8e83bfa48fbde0b1be6a4ccb8cd276cd365fa
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2024-12-25 21:12:38 +0700

    done?

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 18:47:57 by scharuka          #+#    #+#             */
-/*   Updated: 2024/12/25 20:25:13 by scharuka         ###   ########.fr       */
+/*   Updated: 2024/12/25 21:11:48 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,27 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &copy)
 
 void ScalarConverter::convert(const std::string &str)
 {
+	if (caseSciNotation(str))
+		return;
+	static double _double = std::stod(str);
+	static float _float = static_cast<float>(_double);
+	static int _int = static_cast<int>(_double);
+	static char _char = static_cast<char>(_double);
+
+	if (_char < 32 || _char > 126)
+	{
+		std::cout << "char: Non displayable" << std::endl;
+	}
+	else
+	{
+		std::cout << "char: " << _char << std::endl;
+	}
+	std::cout << "int: " << _int << std::endl;
+	std::cout << "float: " << _float << "f" << std::endl;
+	std::cout << "double: " << _double << std::endl;
 }
 
-bool caseSciNotation(std::string &str)
+bool caseSciNotation(const std::string &str)
 {
 	if (str.compare("+inff") || str.compare("+inf"))
 	{
