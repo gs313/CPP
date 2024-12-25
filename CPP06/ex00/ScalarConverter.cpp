4f6bc5af97f639fd1ef9b22fc7c2b5b46a02264d
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2024-12-25 21:42:25 +0700

    done ex00

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,12 +6,13 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 18:47:57 by scharuka          #+#    #+#             */
-/*   Updated: 2024/12/25 21:22:57 by scharuka         ###   ########.fr       */
+/*   Updated: 2024/12/25 21:41:49 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 bool caseSciNotation(const std::string &str);
+bool caseNotNumber(const std::string &str);
 ScalarConverter::ScalarConverter()
 {
 
@@ -36,10 +37,17 @@ void ScalarConverter::convert(const std::string &str)
 {
 	if (caseSciNotation(str))
 		return;
-	static double _double = std::stod(str);
-	static float _float = static_cast<float>(_double);
-	static int _int = static_cast<int>(_double);
-	static char _char = static_cast<char>(_double);
+	static char _char = str[0];
+	static int _int = static_cast<int>(_char);
+	static float _float = static_cast<float>(_char);
+	static double _double = static_cast<double>(_char);
+	if (!caseNotNumber(str))
+	{
+		_double = std::stod(str);
+		_float = static_cast<float>(_double);
+		_int = static_cast<int>(_double);
+		_char = static_cast<char>(_double);
+	}
 
 	if (_char < 32 || _char > 126)
 	{
@@ -47,11 +55,11 @@ void ScalarConverter::convert(const std::string &str)
 	}
 	else
 	{
-		std::cout << "char: " << _char << std::endl;
+		std::cout << "char: '" << _char << "'" << std::endl;
 	}
 	std::cout << "int: " << _int << std::endl;
-	std::cout << std::setprecision(1) << "float: " << _float << "f" << std::endl;
-	std::cout << std::setprecision(1) << "double: " << _double << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "float: " << _float << "f" << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "double: " << _double << std::endl;
 }
 
 bool caseSciNotation(const std::string &str)
@@ -74,7 +82,7 @@ bool caseSciNotation(const std::string &str)
 		return (true);
 	}
 
-	else if (!str.compare("nanf"))
+	else if (!str.compare("nanf") || !str.compare("nan"))
 	{
 		std::cout << "char: impossible" << std::endl;
 		std::cout << "int: impossible" << std::endl;
@@ -86,4 +94,31 @@ bool caseSciNotation(const std::string &str)
 	{
 		return (false);
 	}
+}
+bool caseNotNumber(const std::string &str)
+{
+	int i = 0;
+	bool nodot = 1;
+	bool nof = 1;
+	if (str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (str[i] == '.' && nodot)
+		{
+			nodot = 0;
+			i++;
+			continue;
+		}
+		else if (str[i] == 'f' && nof)
+		{
+			nof = 0;
+			i++;
+			continue;
+		}
+		else if (!std::isdigit(str[i]))
+			return (true);
+		i++;
+	}
+	return (false);
 }
