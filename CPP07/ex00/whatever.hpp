0f16b6f412d613379c55879b9592d8e7a21b7759
gs313 <sirapat1charu@gmail.com>
2025-05-11 11:36:14 +0700

    cpp07 ex00

--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 20:57:27 by scharuka          #+#    #+#             */
-/*   Updated: 2025/05/11 11:27:01 by scharuka         ###   ########.fr       */
+/*   Updated: 2025/05/11 11:32:20 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,19 @@
 template <typename T>
 void swap(T &a, T &b)
 {
-	T temp = x;
-	x = y;
-	y = temp;
+	T temp = a;
+	a = b;
+	b = temp;
 }
 
 template <typename P>
-P min ()
+const P& min(const P& a, const P&b)
+{
+	return (a < b ? a : b);
+}
+template <typename G>
+const G& max (const G& a,const G& b)
+{
+	return (a > b ? a :b);
+}
 #endif
