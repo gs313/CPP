4a7ae7042b80cece76c2a9dfcfa1d7f1ba3ad454
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2025-05-16 16:08:48 +0700

    cp007

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scharuka <scharuka@42.fr>                  +#+  +:+       +#+        */
+/*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 11:40:58 by scharuka          #+#    #+#             */
-/*   Updated: 2025/05/11 11:40:59 by scharuka         ###   ########.fr       */
+/*   Updated: 2025/05/16 15:52:39 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+int main( void ) {
+	int arr[] = { 1, 2, 3, 4, 5 };
+	std::string strArr[] = { "Hello", "World", "!" };
+
+	iter(arr, 5, [](int& n) { n *= 2; });
+	iter(strArr, 3, [](std::string& str) { str += "!"; });
+
+	for (int i = 0; i < 5; ++i)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+
+	for (int i = 0; i < 3; ++i)
+		std::cout << strArr[i] << " ";
+	std::cout << std::endl;
+
+	return 0;
+	}
